6c108814099a5af14d5af4dcc018056de3a79024
Socret Lee <socretlee@Socrets-MacBook-Pro.local>
2020-03-20 21:13:48 +0700

    added base data generator

---
+++ base/BaseDataGenerator.py
@@ -0,0 +1,9 @@
+class BaseDataGenerator(tf.keras.utils.Sequence):
+  def __init__(self):
+    """"""
+
+  def __len__(self):
+    raise NotImplementedError
+
+  def __getitem__(self, idx):
+    raise NotImplementedError
