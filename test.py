c2bf052988b6e12bba3bc283d17e62cd67582eb8
Socret Lee <socretlee@Socrets-MacBook-Pro-2.local>
2020-12-31 10:20:48 +0700

    adjust ssd300_mobilenetv2_coco2017-train.json to generate smaller default boxes

--- test.py
+++ test.py
@@ -44,7 +44,7 @@ from networks import SSD300_MOBILENET_V2
 with open("configs/ssd300_mobilenetv2.json", "r") as config_file:
     config = json.load(config_file)
 
-with open("/Users/socretlee/Downloads/ssd300_vgg16_voc-07-12_20-classes_118-epochs_label_maps.txt", "r") as file:
+with open("/Users/socretlee/Downloads/fast-ai-coco/label_maps.txt", "r") as file:
     label_maps = [line.strip("\n") for line in file.readlines()]
 
 model = SSD300_MOBILENET_V2(
