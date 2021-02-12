53f54a9a0c57a75b4e0dabae3485cbef9ac0f859
Socret360 <lee.socret@gmail.com>
2021-02-12 11:52:04 +0700

    working on adding textboxes plus plus

--- data_generators/__init__.py
+++ data_generators/__init__.py
@@ -1,1 +1,2 @@
 from .ssd_data_generator import SSD_DATA_GENERATOR
+from .tbpp_data_generator import TBPP_DATA_GENERATOR
--- test.py
+++ test.py
@@ -1,54 +1,8 @@
-# import cv2
-# import os
-# from networks import SSD300_MOBILENET_V2
-# from pycocotools.coco import COCO
-
-# annotations_path = "/Users/socretlee/Downloads/fast-ai-coco/annotations/"
-# images_path = "/Users/socretlee/Downloads/fast-ai-coco/train2017/"
-# coco = COCO(os.path.join(annotations_path, "instances_train2017.json"))
-
-# cats = coco.cats
-
-# for image_id in coco.imgs.keys():
-#     image_info = coco.loadImgs([image_id])
-#     annotations = coco.loadAnns(coco.getAnnIds([image_id]))
-#     image = cv2.imread(os.path.join(images_path, image_info[0]["file_name"]))
-#     for annotation in annotations:
-#         bbox = annotation['bbox']
-#         category_id = annotation['category_id']
-#         cv2.rectangle(
-#             image,
-#             (int(bbox[0]), int(bbox[1])),
-#             (int(bbox[0] + bbox[2]), int(bbox[1] + bbox[3])),
-#             (0, 255, 0),
-#             1
-#         )
-#         cv2.putText(
-#             image,
-#             coco.cats[category_id]["name"],
-#             (int(bbox[0]), int(bbox[1])),
-#             cv2.FONT_HERSHEY_SIMPLEX,
-#             1,
-#             (0, 0, 255),
-#             1
-#         )
-
-#     cv2.imshow('image', image)
-#     if cv2.waitKey(0) == ord('q'):
-#         cv2.destroyAllWindows()
-
-#     exit()
 import json
-from networks import SSD_MOBILENETV2
+from networks import TBPP_VGG16
 
-with open("configs/ssd320_mobilenetv2_coco2017-train.json", "r") as config_file:
+with open("configs/tbpp300_vgg16.json", "r") as config_file:
     config = json.load(config_file)
 
-with open("/Users/socretlee/Downloads/fast-ai-coco/label_maps.txt", "r") as file:
-    label_maps = [line.strip("\n") for line in file.readlines()]
-
-model = SSD_MOBILENETV2(
-    config,
-    label_maps=label_maps
-)
+model = TBPP_VGG16(config)
 model.summary()
