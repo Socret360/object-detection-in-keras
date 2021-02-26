9da780766c1f97852dde4dc24f828b1079b4fd1f
Socret360 <lee.socret@gmail.com>
2021-02-26 09:49:22 +0700

    plot log every n iterations or epochs

--- callbacks/model_checkpoint.py
+++ callbacks/model_checkpoint.py
@@ -17,28 +17,28 @@ class ModelCheckpoint(Callback):
         if self.iteration_frequency is not None:
             loss = logs["loss"]
             self.losses_by_iteration.append(loss)
-            plt.plot(list(range(1, self.iterations+1)), self.losses_by_iteration)
-            plt.title('training loss')
-            plt.ylabel('loss')
-            plt.xlabel('iteration')
-            plt.savefig(os.path.join(self.output_dir, "log.png"))
             if self.iterations % self.iteration_frequency == 0:
                 loss = '%.2f' % loss
                 name = f"cp_it_{self.iterations}_loss_{loss}.h5"
                 self.model.save_weights(os.path.join(self.output_dir, name))
+                plt.plot(list(range(1, self.iterations+1)), self.losses_by_iteration)
+                plt.title('training loss')
+                plt.ylabel('loss')
+                plt.xlabel('iteration')
+                plt.savefig(os.path.join(self.output_dir, "log.png"))
         self.iterations += 1
 
     def on_epoch_end(self, epoch, logs={}):
         if self.epoch_frequency is not None:
             loss = logs["loss"]
             self.losses_by_epoch.append(loss)
-            plt.plot(list(range(1, self.epochs+1)), self.losses_by_epoch)
-            plt.title('training loss')
-            plt.ylabel('loss')
-            plt.xlabel('epoch')
-            plt.savefig(os.path.join(self.output_dir, "log.png"))
             if self.epochs % self.epoch_frequency == 0:
                 loss = '%.2f' % loss
                 name = f"cp_ep_{self.epochs}_loss_{loss}.h5"
                 self.model.save_weights(os.path.join(self.output_dir, name))
+                plt.plot(list(range(1, self.epochs+1)), self.losses_by_epoch)
+                plt.title('training loss')
+                plt.ylabel('loss')
+                plt.xlabel('epoch')
+                plt.savefig(os.path.join(self.output_dir, "log.png"))
         self.epochs += 1
